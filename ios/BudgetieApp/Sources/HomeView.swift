08251382ffeabda4cd7b715e1260edf8f081009c
Kris <christina.matziouni@atcom.gr>
2025-08-08 23:31:02 +0300

    restore homeView

--- ios/BudgetieApp/Sources/HomeView.swift
+++ ios/BudgetieApp/Sources/HomeView.swift
@@ -7,35 +7,20 @@
 
 import SwiftUI
 import AppLogging
-import OnboardingAPI
 
 struct HomeView: View {
     @Injected private var logger: BTLogger
-    @Injected private var shouldShowOnboardingUC: ShouldShowOnboardingUC
     
     var body: some View {
-        if shouldShowOnboardingUC.execute() {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, onboarding!")
-            }
-            .padding()
-            .onAppear {
-                logger.log(.debug, fileName: "ContentView", "Onboarding appeared ✅")
-            }
-        } else {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
-            .padding()
-            .onAppear {
-                logger.log(.debug, fileName: "ContentView", "ContentView appeared ✅")
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            logger.log(.debug, fileName: "ContentView", "ContentView appeared ✅")
         }
         
     }
